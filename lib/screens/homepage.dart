674745369c9bbfc4b8b6e761888dd979a4175ebe
Skolas3654 <a3654058@gmail.com>
2023-10-13 18:24:41 -0400

    -You can now add a task from the AddTask widget

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:MotiList/utils/reusable_widget.dart';
 import 'package:MotiList/screens/login_page.dart';
 
+// Import or define the TodoItem widget as well as the WeekCheckbox widget
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -10,124 +12,24 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class CheckboxExampleApp extends StatelessWidget {
-  const CheckboxExampleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Checkbox Sample')),
-        body: const Center(
-          child: CheckboxExample(),
-        ),
-      ),
-    );
-  }
-}
-
-class CheckboxExample extends StatefulWidget {
-  const CheckboxExample({super.key});
-
-  @override
-  State<CheckboxExample> createState() => _CheckboxExampleState();
-}
-
-class _CheckboxExampleState extends State<CheckboxExample> {
-  bool isChecked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    Color getColor(Set<MaterialState> states) {
-      const Set<MaterialState> interactiveStates = <MaterialState>{
-        MaterialState.pressed,
-        MaterialState.hovered,
-        MaterialState.focused,
-      };
-      if (states.any(interactiveStates.contains)) {
-        return Colors.blue;
-      }
-      return Colors.red;
-    }
-
-    return Checkbox(
-      checkColor: Colors.white,
-      fillColor: MaterialStateProperty.resolveWith(getColor),
-      value: isChecked,
-      onChanged: (bool? value) {
-        setState(() {
-          isChecked = value!;
-        });
-      },
-    );
-  }
-}
-
-void _showAddTaskBottomSheet(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    builder: (context) {
-      return Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize:
-              MainAxisSize.min, // Ensures only necessary space is taken
-          children: [
-            const Text('Add New Task', style: TextStyle(fontSize: 20)),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Task Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-              WeekCheckbox(char: "M"),
-              WeekCheckbox(char: 'T'),
-              WeekCheckbox(char: 'W'),
-              WeekCheckbox(char: 'TH'),
-              WeekCheckbox(char: 'F'),
-              WeekCheckbox(char: "SA"),
-              WeekCheckbox(char: 'SU'),
-            ]),
-            const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
-                  labelText: 'Task Description',
-                  border: OutlineInputBorder(),
-                  contentPadding: EdgeInsets.all(55)),
-            ),
-            const SizedBox(height: 20),
-            const DropdownButtonExample(),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              child: const Text('Add Task'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      );
-    },
-  );
-}
-
-//This logout button should be moved to the profile page or settings
 class _HomeScreenState extends State<HomeScreen> {
-  List<String> todoItems = [];
+  final List<TodoItem> todoItems = [];
+
+  void addTodoItem(TodoItem item) {
+    setState(() {
+      todoItems.add(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: const Text("Be productive you GOOBER"), centerTitle: true),
       floatingActionButton: FloatingActionButton(onPressed: () {
-        todoItems.add('New Todo ${todoItems.length + 1}');
-        _showAddTaskBottomSheet(context);
-        print(todoItems);
+        _showAddTaskBottomSheet(context, addTodoItem);
       }),
       body: Container(
-        //alignment: Alignment.center,
         padding: const EdgeInsets.all(8.0),
         margin: const EdgeInsets.all(5),
         decoration: BoxDecoration(
@@ -136,24 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: [
-            TodoItem(
-              text: 'Learn Flutter',
-              categoryIcon: Icons.school,
-              description: 'Complete the Flutter course on XYZ platform.',
-              date: DateTime.now().add(const Duration(days: 2)),
-            ),
-            TodoItem(
-              text: 'Kill a group member',
-              categoryIcon: Icons.school,
-              description: 'They will never know',
-              date: DateTime.now().add(const Duration(days: 2)),
-            ),
-            TodoItem(
-              text: 'Eat',
-              categoryIcon: Icons.school,
-              description: 'Complete the Flutter course on XYZ platform.',
-              date: DateTime.now().add(const Duration(days: 2)),
-            ),
+            ...todoItems,
             Align(
               alignment: Alignment.bottomLeft,
               child: ElevatedButton(
@@ -175,4 +60,70 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
+
+void _showAddTaskBottomSheet(
+    BuildContext context, Function(TodoItem) addTodoItem) {
+  final TitleController = TextEditingController();
+  final DescController = TextEditingController();
+
+  showModalBottomSheet(
+    context: context,
+    builder: (context) {
+      return Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text('Add New Task', style: TextStyle(fontSize: 20)),
+            const SizedBox(height: 20),
+            TextField(
+              controller: TitleController,
+              decoration: const InputDecoration(
+                labelText: 'Task Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Ensure you have the WeekCheckbox widget defined elsewhere
+                WeekCheckbox(char: "M"),
+                WeekCheckbox(char: 'T'),
+                WeekCheckbox(char: 'W'),
+                WeekCheckbox(char: 'TH'),
+                WeekCheckbox(char: 'F'),
+                WeekCheckbox(char: "SA"),
+                WeekCheckbox(char: 'SU'),
+              ],
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: DescController,
+              decoration: const InputDecoration(
+                  labelText: 'Task Description',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.all(55)),
+            ),
+            const SizedBox(height: 20),
+            // Ensure you have the DropdownButtonExample widget defined elsewhere
+            const DropdownButtonExample(),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              child: const Text('Add Task'),
+              onPressed: () {
+                final newItem = TodoItem(
+                  text: TitleController.text,
+                  categoryIcon: Icons.abc,
+                  description: DescController.text,
+                );
+                addTodoItem(newItem);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      );
+    },
+  );
 }
