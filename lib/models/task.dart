bb38b9cbb24ffa79f7d0dbc382218f8027853acb
Spencerhoag <97331670+Spencerhoag@users.noreply.github.com>
2023-11-21 14:05:10 -0500

    Update task.dart
    works with a string-bool map now for days of the week

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -5,23 +5,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Task {
-  late DateTime dateTime;
+  late Map<String, bool> daysOfWeek;
   late String title;
   late String description;
   late String category;
 
 ///Task object
-  /// @param dateTime The date and time by which a task should be completed
+  /// @param daysOfWeek The days of the week a task is assigned to
   /// @param title The name of a given task
   /// @param description The description of a task, currently no text limit
   /// @param category The list to which a task belong, category(s) are of a limited type
-  Task({required this.dateTime, required this.title,
+  Task({required this.daysOfWeek, required this.title,
         required this.description, required this.category});
 ///
   /// Convert to map
   Map<String, dynamic> toMap() {
     return {
-      'dateTime': Timestamp.fromDate(dateTime),
+      'daysOfWeek': daysOfWeek,
       'title': title,
       'description': description,
       'category': category,
@@ -31,7 +31,8 @@ class Task {
   ///
   static Task fromMap(Map<String, dynamic> map) {
     return Task(
-      dateTime: (map['dateTime'] as Timestamp).toDate(),
+      daysOfWeek: (map['daysOfWeek'] as Map<String, dynamic>)
+      .map((key, value) => MapEntry(key, value as bool)),
       title: map['title'],
       description: map['description'],
       category: map['category'],
