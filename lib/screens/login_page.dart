c45abc63ccfc12c806e9b52ad36f5fd0d736fac7
Skolas3654 <a3654058@gmail.com>
2023-11-27 17:36:50 -0500

    -Fixed overflowing pixel bug on mobile

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -50,7 +50,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+        body: SingleChildScrollView(
+      child: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: BoxDecoration(
@@ -178,7 +179,7 @@ class _LoginPageState extends State<LoginPage> {
           ],
         ),
       ),
-    );
+    ));
   }
 
   Row signUpOption() {
