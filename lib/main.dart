d44344798047b9f7480ea1eaa3817f297a647247
Joshua Uys <joshua.luke.uys@gmail.com>
2023-11-27 11:56:52 -0500

    Fixed route map error
    Added login page to be default home

--- lib/main.dart
+++ lib/main.dart
@@ -84,7 +84,6 @@ class _MyAppState extends State<MyApp> {
         ),
         initialRoute: '/', // Set the initial route
         routes: {
-          '/': (context) => const LoginPage(),
           '/profile': (context) => const ProfileScreen(),
           '/homepage': (context) => const ProfileScreen(),
         },
